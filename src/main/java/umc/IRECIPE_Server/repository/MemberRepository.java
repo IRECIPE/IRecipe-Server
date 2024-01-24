317c10d1628ead7691f228f418b024c4bd5d170e
jun23314 <dlwnsfml@naver.com>
2024-01-24 17:10:40 +0900

    login 구현, swagger 설정

---
+++ src/main/java/umc/IRECIPE_Server/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package umc.IRECIPE_Server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.IRECIPE_Server.domain.mapping.Member;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findById(String username);
+}
