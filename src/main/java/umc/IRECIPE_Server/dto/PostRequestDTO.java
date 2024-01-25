5e8f4301c65b74dfc6e8a575c2f54f3a28245826
minjae <wjsalswp303@gmail.com>
2024-01-26 00:31:28 +0900

    [feat] 게시글 단일 조회 구현

--- src/main/java/umc/IRECIPE_Server/dto/PostRequestDTO.java
+++ src/main/java/umc/IRECIPE_Server/dto/PostRequestDTO.java
@@ -1,13 +1,16 @@
 package umc.IRECIPE_Server.dto;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import umc.IRECIPE_Server.common.enums.Category;
 import umc.IRECIPE_Server.common.enums.Level;
 import umc.IRECIPE_Server.common.enums.Status;
 
 import java.util.List;
 
-@Data
+@Getter
+@Builder
 public class PostRequestDTO {
 
     private String title;
--- src/main/java/umc/IRECIPE_Server/service/PostService.java
+++ src/main/java/umc/IRECIPE_Server/service/PostService.java
@@ -11,7 +11,9 @@ import umc.IRECIPE_Server.dto.PostRequestDTO;
 import umc.IRECIPE_Server.dto.PostResponseDTO;
 import umc.IRECIPE_Server.entity.Member;
 import umc.IRECIPE_Server.entity.Post;
+import umc.IRECIPE_Server.entity.PostImage;
 import umc.IRECIPE_Server.repository.MemberRepository;
+import umc.IRECIPE_Server.repository.PostImageRepository;
 import umc.IRECIPE_Server.repository.PostRepository;
 
 import java.util.Optional;
@@ -23,10 +25,12 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final MemberRepository memberRepository;
+    private final PostImageRepository postImageRepository;
 
-    // requestDto 를 받아 게시글 저장 후 responseDto 생성해서 반환하는 메소드.
+    // PostRequestDto 를 받아 DB에 게시글 저장 후 PostResponseDto 생성해서 반환하는 메소드.
+    // 게시글 생성 (Create)
     @Transactional // Read only 해제
-    public ApiResponse<?> posting(String userId, PostRequestDTO postRequestDto){
+    public ApiResponse<?> posting(String userId, PostRequestDTO postRequestDto, String url){
 
         // 리포지토리에서 멤버 찾기.
         Optional<Member> memberOptional = memberRepository.findById(userId);
@@ -46,15 +50,67 @@ public class PostService {
                 .status(postRequestDto.getStatus())
                 .build();
 
+        // 게시글 사진 객제 생성
+        PostImage postImage = PostImage.builder()
+                .post(post)
+                .imageUrl(url)
+                .build();
 
         // 게시글 저장
         postRepository.save(post);
 
+        // 게시글 사진 저장
+        postImageRepository.save(postImage);
+
         // responseDTO 생성
-        PostResponseDTO postResponseDTO = new PostResponseDTO();
-        postResponseDTO.setPostId(post.getId());
+        PostResponseDTO postResponseDTO = PostResponseDTO.builder()
+                .postId(post.getId())
+                .build();
 
         // 응답 반환.
         return ApiResponse.onSuccess(postResponseDTO);
+    }
+
+    // 게시글 단일 조회 (Read)
+    public ApiResponse<?> getPost(Long postId){
+
+        // 리포지토리에서 게시글 찾기. 없으면 에러 출력.
+        Optional<Post> postOptional = postRepository.findById(postId);
+        Post post;
+
+        if(postOptional.isEmpty()){
+            throw new GeneralException(ErrorStatus.POST_NOT_FOUND);
+        }
+        post = postOptional.get();
+
+        // post 객체로 postImage 찾기
+        Optional<PostImage> postImageOptional = postImageRepository.findByPost(post);
+        PostImage postImage;
+        String imageUrl;
+
+        if(postImageOptional.isEmpty()){
+            imageUrl = null;
+        }
+        else{
+            postImage = postImageOptional.get();
+            imageUrl = postImage.getImageUrl();
+        }
+
+        // PostResponseDTO 생성
+        PostResponseDTO postResponseDTO = PostResponseDTO.builder()
+                .postId(post.getId())
+                .title(post.getTitle())
+                .subhead(post.getSubhead())
+                .category(post.getCategory())
+                .content(post.getContent())
+                .level(post.getLevel())
+                .likes(post.getLikes())
+                .score(post.getScore())
+                .status(post.getStatus())
+                .url(imageUrl)
+                .build();
+
+        return ApiResponse.onSuccess(postResponseDTO);
+
     }
 }
